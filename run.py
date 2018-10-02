e0066be9acd7c16817392d44b12f8d15ef6fa3ba
ARUNCO <duncanarani@gmail.com>
2018-10-02 17:27:48 +0300

    done

---
+++ run.py
@@ -0,0 +1,6 @@
+from app import app 
+
+
+if __name__ == 'main':
+    app.run()
+    # use the app.run() method to launch our server. We pass in the debug = Trueargument to allow us to run on debug mode so that we can easily track errors in our application.
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY=4fe63f110faa162009b4f15f5693155e
+export SECRET_KEY=Arani
+
+python3.6 manage.py server

d51ac770bb09873b80d694b16903b5f009f29c11
ARUNCO <duncanarani@gmail.com>
2018-10-11 10:11:34 +0300

    sign in

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MOVIE_API_KEY=4fe63f110faa162009b4f15f5693155e
 export SECRET_KEY=Arani
+export MAIL_USERNAME=aruncodunco@gmail.com
+export MAIL_PASSWORD=31740141
 
 python3.6 manage.py server

980ecf98353d266adea606494b6deb253c859a66
ARUNCO <duncanarani@gmail.com>
2018-10-11 21:58:54 +0300

    deployement

--- config.py
+++ config.py
@@ -10,12 +10,15 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
-    #email configurations
+    #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Watchlist'
+    SENDER_EMAIL = 'aruncodunco@gmail.com'
+
 
 
     @staticmethod
@@ -24,7 +27,8 @@ class Config:
 
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
 
 
 class TestConfig(Config):
